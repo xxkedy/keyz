c9edf20a73ed0a9b2ab4074c7bf1274007d328f7
xxkedy <subaka2382@gmail.com>
2026-07-17 16:35:28 +0900

    Bump Appz cache for final icons

--- appz/sw.js
+++ appz/sw.js
@@ -1,4 +1,4 @@
-const CACHE='appz-v1';
+const CACHE='appz-v2';
 const LOCAL=['./','./index.html','./manifest.json','../icon-192.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
