748da7a14b7e2a57f2064ea8a4f78ae389eb58aa
xxkedy <subaka2382@gmail.com>
2026-09-03 23:33:38 +0900

    Update Appz v9.5 service worker cache

--- appz/sw.js
+++ appz/sw.js
@@ -1,5 +1,5 @@
-const CACHE='appz-v9.4-pwa-id-20260903';
-const LOCAL=['./','./index.html','./manifest.json?v=20260903-1','./icon-180.png?v=20260729-1','./icon-192.png?v=20260729-1','./icon-512.png?v=20260729-1'];
+const CACHE='appz-v9.5-desktop-20260903';
+const LOCAL=['./','./index.html','./manifest.json?v=20260903-2','./icon-180.png?v=20260729-1','./icon-192.png?v=20260729-1','./icon-512.png?v=20260729-1'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('appz-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(new URL(event.request.url).origin!==location.origin)return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./'))))});
