83f8313d17e53a07a66bfa181cfcc286a67b1af8
xxkedy <subaka2382@gmail.com>
2026-09-06 01:31:21 +0900

    chore: bump Appz service worker cache for v10.1

--- appz/sw.js
+++ appz/sw.js
@@ -1,4 +1,4 @@
-const CACHE='appz-v10.0-20260904';
+const CACHE='appz-v10.1-20260906';
 const LOCAL=['./','./index.html','./manifest.json?v=20260904-1','./now.json','./icon-180.png?v=20260729-1','./icon-192.png?v=20260729-1','./icon-512.png?v=20260729-1'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('appz-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
