3c3cec455d0184abccb614eaedf72b36fd10df5c
kedy <xxkedy611@gmail.com>
2026-09-08 00:15:11 +0900

    fix(pwa): refresh network assets during version updates

--- appz/sw.js
+++ appz/sw.js
@@ -1,11 +1,11 @@
 const CACHE='appz-v11.0-20260907';
 const LOCAL=['./','./index.html','./manifest.json?v=20260904-1','./icon-180.png?v=20260729-1','./icon-192.png?v=20260729-1','./icon-512.png?v=20260729-1'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL)).then(()=>self.skipWaiting()))});
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL.map(url=>new Request(url,{cache:'reload'})))).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('appz-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url),scope=new URL(self.registration.scope);
   if(event.request.method!=='GET'||url.origin!==scope.origin||!url.pathname.startsWith(scope.pathname))return;
-  event.respondWith(fetch(event.request).then(response=>{
+  event.respondWith(fetch(event.request,{cache:'reload'}).then(response=>{
     if(response.ok){const copy=response.clone();event.waitUntil(caches.open(CACHE).then(cache=>cache.put(event.request,copy)))}
     return response;
   }).catch(async()=>{
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const C = 'keyz-v13-deeplink-20260907';
 const ASSETS = ['./', './index.html', './manifest.json?v=20260903-1', './icon-192.png?v=20260718-3', './icon-512.png?v=20260718-3'];
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS.map(url=>new Request(url,{cache:'reload'})))).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k.startsWith('keyz-') && k !== C).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
